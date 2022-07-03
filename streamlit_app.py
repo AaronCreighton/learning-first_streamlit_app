5a5d101a6d5ea30b89f0159406e0a6e8e74d1dd8
Aaron Creighton <aaron.creighton@jnctn.nz>
2022-07-04 11:35:58 +1200

    fetch all

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,7 +55,8 @@ my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()") #return account info
 my_cur.execute("Select * from fruit_load_list")
-my_data_row = my_cur.fetchone()
+#my_data_row = my_cur.fetchone()
+my_data_rows = my_cur.fetchall()
 st.header("the fruit Load list contains:")
 st.dataframe(my_data_row)
 
