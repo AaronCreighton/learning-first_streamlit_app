88acdd9102784e03e622b8460f1ec89709ff3481
Aaron Creighton <aaron.creighton@jnctn.nz>
2022-07-03 16:38:01 +1200

    tesing 1.0

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,9 +52,9 @@ import snowflake.connector
 
 # Query account metadata
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-st.text("Hello from Snowflake:")
-st.text(my_data_row)
+#my_cur = my_cnx.cursor()
+#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+#my_data_row = my_cur.fetchone()
+#st.text("Hello from Snowflake:")
+#st.text(my_data_row)
 
