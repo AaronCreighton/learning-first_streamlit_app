8e657477912d6502574ba5c79a52212bc7f171fe
Aaron Creighton <aaron.creighton@jnctn.nz>
2022-07-10 15:00:58 +1200

    enter fruit bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -84,13 +84,14 @@ def insert_row_snowflake(new_fruit):
         my_cur.execute("insert into fruit_load_list values ('"+ new_fruit +"')")
         return "Thanks for adding " + new_fruit
 
+# Empty table
 def truncate_row_snowflake():
     with my_cnx.cursor() as my_cur:
         my_cur.execute("truncate fruit_load_list")
         return "Thanks for Emplying the list"
      
-add_wanted_fruit = st.text_input('What fruit would you like to add?','jackfruit')
-add_wanted_fruit_low = add_wanted_fruit.lower()
+add_my_fruit = st.text_input('What fruit would you like to add?','jackfruit')
+add_wanted_fruit_low = add_my_fruit.lower()
 my_availble_fruit = str(my_fruit_list.index).lower()
 
 if add_wanted_fruit_low in my_availble_fruit:
@@ -99,7 +100,7 @@ else:
         
     if st.button('add a Fruit to the List'):
         my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
-        back_from_function = insert_row_snowflake(add_wanted_fruit)
+        back_from_function = insert_row_snowflake(add_my_fruit)
         my_cnx.close()
         st.text(back_from_function)
 
